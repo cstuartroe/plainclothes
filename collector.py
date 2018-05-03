103f06642950dd6612a516d74e8d0265c5d5d868
Conor <cstuartroe@haverford.edu>
2018-05-03 00:04:17 -0400

    add scraper and collector

---
+++ collector.py
@@ -0,0 +1,67 @@
+import os
+import string
+import time
+from urllib import request as ur
+from urllib import parse as up
+from bs4 import BeautifulSoup as bs
+import lxml
+
+#from wikiscraper import *
+from replacements import *
+
+included_chs = string.ascii_letters + string.digits + string.punctuation + ' £€¥₹฿₣₱₽₩'
+
+def unicode_lookup(symbol):
+    url = 'http://graphemica.com/' + up.quote(symbol)
+    req = ur.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    document = ur.urlopen(req).read()
+    soup = bs(document, 'lxml')
+    return soup.title.text
+
+def write_replacements():
+    with open('replacements.py','w',encoding='utf-8') as fh:
+        fh.write('ascii_replacements = ' + str(ascii_replacements))
+
+def collect():
+    start_time = time.time()
+    corpus = ''
+    folder = 'sources'
+    articles = os.listdir(folder)
+    
+    for the_file in articles:
+        file_path = os.path.join(folder, the_file)
+        if os.path.isfile(file_path):
+            print('Transferring %s...' % the_file)
+            with open(file_path,'r',encoding='utf-8') as fh:
+                text = fh.read() + ' '
+            for ch in text:
+                try:
+                    newseq = ascii_replacements[ch]
+                    assert(all(newch in included_chs for newch in newseq))
+                    corpus += newseq
+                except KeyError:
+                    try:
+                        print('Unknown Char: ' + unicode_lookup(ch))
+                        replacement = input('Replacement: ')
+                        if replacement == 'exit':
+                            write_replacements()
+                            print('%d entries in ascii_replacements.' % len(ascii_replacements.keys()))
+                            raise KeyError
+                        corpus += replacement
+                        ascii_replacements[ch] = replacement   
+                    except UnicodeEncodeError:
+                        pass
+                except AssertionError:
+                    print(ch)
+                    write_replacements()
+                    raise AssertionError
+            write_replacements()
+
+    print('Collected %d sources.' % len(articles))
+    print('Corpus has %d characters.' % len(corpus))
+    with open('corpus.txt','w',encoding='utf-8') as fh:
+        fh.write(corpus)
+    elapsed = int(time.time() - start_time)
+    print('Took %d seconds.' % elapsed)
+
+collect()
---
+++ wikiscraper.py
@@ -0,0 +1,71 @@
+from urllib import request as ur
+from bs4 import BeautifulSoup as bs
+import lxml
+import re
+import os
+import time
+
+articles = ['China','India','United_States','Indonesia','Pakistan','Brazil','Nigeria','Bangladesh',
+            'Russia','Japan','Mexico','Philippines','Egypt','Ethiopia','Vietnam','Germany','Iran',
+            'Democratic_Republic_of_the_Congo','Turkey','Thailand','United_Kingdom','France',
+            'Italy','Canada','South_Korea','Australia','Spain','Netherlands','Saudi_Arabia','Switzerland',
+            'European_Union',
+            'Tokyo','Yokohama','Jakarta','Delhi','Manila','Seoul','Karachi','Shanghai','Mumbai',
+            'New_York_City','S%C3%A3o_Paulo','Beijing','Mexico_City','Guangzhou','Foshan',
+            'Osaka','Dhaka','Moscow','Cairo','Bangkok','Los_Angeles','Buenos_Aires','Kolkata',
+            'Tehran','Istanbul','Lagos','Tianjin','Shenzhen','Rio_de_Janeiro','Kinshasa',
+            'Lima','Chengdu','Paris','Lahore','Bangalore','London','Ho_Chi_Minh_City','Chennai',
+            'Nagoya','Bogot%C3%A1','Hyderabad','Chicago','Johannesburg','Taipei',
+            'Facebook','Youtube','Google','Wikipedia','Apple_Inc.','Microsoft','Amazon.com',
+            'Donald_Trump','Barack_Obama','Adolf_Hitler','Abraham_Lincoln','Theresa_May',
+            'Narendra_Modi','Chuck_Schumer','Julian_Assange','James_Comey','Kim_Jong_Un',
+            'Xi_Jinping','Rodrigo_Duterte','Vladimir_Putin','Pope_Francis','Recep_Tayyip_Erdo%C4%9Fan',
+            'Juan_Manuel_Santos','Vajiralongkorn','Qasem_Soleimani',
+            'Lady_Gaga','Eminem','The_Beatles','Steve_Jobs','Elizabeth_II','Cristiano_Ronaldo',
+            'Stephen_Hawking','Lil_Wayne','Rihanna','Selena_Gomez','Taylor_Swift','Lionel_Messi',
+            'Albert_Einstein','Johnny_Depp','Melinda_Gates','Chance_the_Rapper','Constance_Wu',
+            'Janet_Yellen','Lebron_James','Jeff_Bezos','Indra_Nooyi',
+            'Earth','Sun','Moon','Star',
+            'Mathematics','Science','History','Writing',
+            'World_War_I','World_War_II',]
+
+def wikiscrape(article_name):
+    url = 'https://en.wikipedia.org/wiki/' + article_name
+    document = ur.urlopen(url).read()
+    soup = bs(document, 'lxml')
+    body = soup.find('div',{'class':'mw-parser-output'})
+    #body.find('div',{'id':'toc'}).decompose()
+    paras = [para.text for para in body.find_all('p')]
+    text = '\n'.join(paras)
+    text = re.sub(r'\[.*?\]','',text)
+    text = text.replace('\n',' ')
+    text = text.replace('\xa0',' ')
+    re.sub(' +',' ',text)
+
+    with open('sources/Wikipedia_' + article_name + '.txt','w',encoding='utf-8') as fh:
+        fh.write(text)
+
+    return text
+
+def scrape_all(articles):
+    start_time = time.time()
+##    corpus = ''
+    folder = 'sources'
+    for the_file in os.listdir(folder):
+        file_path = os.path.join(folder, the_file)
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+    
+    for article in articles:
+        print('Scraping %s...' % article)
+        wikiscrape(article)
+##        corpus += text + ' '
+
+    print('Scraped %d articles.' % len(articles))
+##    print('Corpus has %d characters.' % len(corpus))
+##    with open('corpus.txt','w',encoding='utf-8') as fh:
+##        fh.write(corpus)
+    elapsed = int(time.time() - start_time)
+    print('Took %d seconds.' % elapsed)
+
+scrape_all(articles)
